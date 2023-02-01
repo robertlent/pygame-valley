727a17c8974c1ec74a409933b3e98d138434d733
Robert Lent <robert@therobertlent.com>
2023-01-31 22:09:14 -0800

    Implement collision detection

--- sprites.py
+++ sprites.py
@@ -9,3 +9,37 @@ class Generic(pygame.sprite.Sprite):
         self.image = surface
         self.rect = self.image.get_rect(topleft=pos)
         self.z = z
+        self.hitbox = self.rect.copy().inflate(-self.rect.width*0.2, -self.rect.height*.75)
+
+
+class Water(Generic):
+    def __init__(self, pos, frames, groups):
+        self.frames = frames
+        self.frame_index = 0
+
+        super().__init__(pos=pos,
+                         surface=self.frames[self.frame_index],
+                         groups=groups,
+                         z=LAYERS['water'])
+
+    def animate(self, dt):
+        self.frame_index += 6 * dt
+
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+
+        self.image = self.frames[int(self.frame_index)]
+
+    def update(self, dt):
+        self.animate(dt)
+
+
+class WildFlower(Generic):
+    def __init__(self, pos, surface, groups):
+        super().__init__(pos, surface, groups)
+        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height*0.9)
+
+
+class Tree(Generic):
+    def __init__(self, pos, surface, groups, name):
+        super().__init__(pos, surface, groups)
