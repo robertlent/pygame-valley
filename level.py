434345fa71cb33923756b7c475780ad76fd53c0d
Robert Lent <robert@therobertlent.com>
2023-02-09 18:21:05 -0800

    Implement plant harvesting
    Walking into a harvestable plant harvests it.
    Harvested plant is added to player's inventory.
    Dead plants are just removed, nothing is added to inventory.

--- level.py
+++ level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree, Interaction
+from sprites import Generic, Water, WildFlower, Tree, Interaction, Particle
 from pytmx.util_pygame import load_pygame
 from support import *
 from debug import draw_hitboxes
@@ -112,10 +112,26 @@ class Level:
         if self.raining:
             self.soil_layer.water_all()
 
+    def plant_collision(self):
+        if self.soil_layer.plant_sprites:
+            for plant in self.soil_layer.plant_sprites.sprites():
+                if plant.harvestable and plant.rect.colliderect(self.player.hitbox):
+                    plant.kill()
+                    self.soil_layer.grid[plant.rect.centery //
+                                         TILE_SIZE][plant.rect.centerx // TILE_SIZE].remove('P')
+
+                    if not plant.plant_dead:
+                        self.add_inventory(plant.plant_type)
+                        Particle(plant.rect.topleft,
+                                 plant.image,
+                                 self.all_sprites,
+                                 LAYERS['main'])
+
     def run(self, dt):
         self.display_surface.fill("black")
         self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
+        self.plant_collision()
 
         self.overlay.display()
 
